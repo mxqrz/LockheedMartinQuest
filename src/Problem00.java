a2f1f30e8e2730628e691135fb5d4e15d68f6ea8
grimflame <iconcosmicalpha@gmail.com>
2023-01-03 18:34:50 -0500

    did some cool problems wooo

---
+++ src/Problem00.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Problem00 {
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in))
+        {
+            int cases = Integer.parseInt(s.nextLine());
+            for (int i = 0; i < cases; i++)
+            {
+                System.out.println(s.nextLine());
+            }
+        }
+    }
+}
---
+++ src/Problem01.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Problem01 {
+    public static void main(String[] args)
+    {
+        try (Scanner s = new Scanner(System.in)) {
+            int intervals = Integer.parseInt(s.nextLine());
+
+            for (int i = 0; i < intervals; i++) {
+                String speeds = s.nextLine();
+
+                double v = Double.parseDouble(speeds.substring(0, speeds.indexOf(":")));
+                double x = Double.parseDouble(speeds.substring(speeds.indexOf(":") + 1));
+
+                if (v == 0) {
+                    System.out.println("SAFE");
+                } else {
+                    double seconds = x / v;
+
+                    if (seconds <= 5) {
+                        if (seconds <= 1) {
+                            System.out.println("SWERVE");
+                        } else {
+                            System.out.println("BRAKE");
+                        }
+                    } else {
+                        System.out.println("SAFE");
+                    }
+                }
+            }
+        }
+    }
+}
