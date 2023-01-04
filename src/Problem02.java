675cfefb19c4ce269b4e039965aa520e8999d9d0
grimflame <iconcosmicalpha@gmail.com>
2023-01-03 19:43:13 -0500

    Problem 2 correct $_$

--- src/Problem02.java
+++ src/Problem02.java
@@ -1,7 +1,20 @@
-// INCOMPLETE
+// CORRECT
 
 import java.util.*;
-import java.io.*;
 
 public class Problem02 {
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int cases = Integer.parseInt(s.nextLine());
+
+        for (int i = 0; i < cases; i++)
+        {
+            String input = s.nextLine();
+            int[] nums = Arrays.stream(input.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            System.out.println((nums[0] + nums[1]) + " " + (nums[0] * nums[1]));
+        }
+    }
 }
