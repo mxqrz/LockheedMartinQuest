a01151c13105a1c8e0da2e68fd6b7d71b309b7bf
mxqrz <104104110+mxqrz@users.noreply.github.com>
2023-01-04 08:41:56 -0500

    Update Problem06.java

--- src/Problem06.java
+++ src/Problem06.java
@@ -1,7 +1,8 @@
-// INCOMPLETE
+// CORRECT
 
 import java.util.*;
 import java.math.*;
+
 public class Problem06 {
     public static void main(String[] args)
     {
@@ -12,17 +13,35 @@ public class Problem06 {
             {
                 int numAsteroids = Integer.parseInt(s.nextLine());
                 ArrayList<int[]> asteroids = new ArrayList<int[]>();
-                ArrayList<Double> distances = new ArrayList<Double>();
-                LinkedHashMap<int[], Double> info = new LinkedHashMap<int[], Double>();
-
 
                 for (int j = 0; j < numAsteroids; j++)
                 {
                     int[] asteroid = Arrays.stream(s.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                    double distance = Math.sqrt(Math.pow(asteroid[0], 2) + Math.pow(asteroid[1], 2));
-
                     asteroids.add(asteroid);
-                    distances.add(distance);
+                }
+                
+                for (int j = 0; j < asteroids.size(); j++)
+                {
+                    for (int k = j + 1; k < asteroids.size(); k++)
+                    {
+                        int[] temp;
+                        
+                        double firstDist = Math.sqrt(Math.pow(asteroids.get(j)[0], 2) + Math.pow(asteroids.get(j)[1], 2));
+                        double secondDist = Math.sqrt(Math.pow(asteroids.get(k)[0], 2) + Math.pow(asteroids.get(k)[1], 2));
+                        
+                        if (firstDist > secondDist)
+                        {
+                            temp = asteroids.get(j);
+                            asteroids.set(j, asteroids.get(k));
+                            asteroids.set(k, temp);
+                        }
+
+                    }
+                }
+                
+                for (int j = 0; j < asteroids.size(); j++)
+                {
+                    System.out.println(asteroids.get(j)[0] + " " + asteroids.get(j)[1]);
                 }
             }
         }
